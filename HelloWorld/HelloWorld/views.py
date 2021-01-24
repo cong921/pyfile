7995ac6a520b711fa133ecf8e85e7728669f366a
lnb <921894@gmail.com>
2021-01-24 22:17:29 +0800

    first commit

---
+++ HelloWorld/HelloWorld/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse("Hello world ! ")
+
+def runoob(request):
+    context          = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'runoob.html', context)
+def halo(request):
+    views_str = "菜鸟教程"
+    return render(request, "runoob.html", {"views_str": views_str})
